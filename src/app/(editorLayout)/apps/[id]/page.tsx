b98e0bfc33d847597a061ec9d09d1a47713af069
Arun <arunprakashrani@gmail.com>
2026-03-06 21:42:42 +0530

    feat: Introduce Suspense for loading states, enhance application store with comprehensive component management, and adjust component schema.

--- src/app/(editorLayout)/apps/[id]/page.tsx
+++ src/app/(editorLayout)/apps/[id]/page.tsx
@@ -1,9 +1,10 @@
 import { Editor } from "@/modules/apps/editor"
+import { Suspense } from "react"
 
 export default function AppById() {
   return (
-    <>
+    <Suspense fallback={<div>Loading Editor...</div>}>
       <Editor />
-    </>
+    </Suspense>
   )
 }
--- src/lib/store/app/state.ts
+++ src/lib/store/app/state.ts
@@ -1,5 +1,13 @@
 import { Application, ApplicationStore } from "@/types/application-store-types"
+import { Component } from "@/types"
 
 export const initialAppState: ApplicationStore = {
   app: {} as Application,
+  selectedComponentId: null,
+  setSelectedComponent: (id: string | null) => {},
+  setApplication: (app: Application) => {},
+  addComponent: (component: Component) => {},
+  updateComponent: (component: Component) => {},
+  updateComponents: (components: Component[]) => {},
+  removeComponent: (id: string) => {},
 }
