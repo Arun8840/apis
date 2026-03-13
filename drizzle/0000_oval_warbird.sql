2e2e2801575574ebe4076ea8dc0fbcbfdba09710
Arun <arunprakashrani@gmail.com>
2026-03-13 16:53:08 +0530

    feat(db): update app-page schema and regenerate migrations

--- drizzle/0000_oval_warbird.sql
+++ drizzle/0000_oval_warbird.sql
@@ -86,7 +86,8 @@ CREATE TABLE "app_page" (
 	"name" text NOT NULL,
 	"description" text NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
-	"updated_at" timestamp DEFAULT now() NOT NULL
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	"style" json DEFAULT '{"backgroundColor":"#000000","backgroundImage":"","backgroundSize":"cover","backgroundPosition":"center","backgroundRepeat":"no-repeat"}'::json
 );
 --> statement-breakpoint
 ALTER TABLE "account" ADD CONSTRAINT "account_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
